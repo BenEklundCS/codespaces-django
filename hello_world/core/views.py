72daaa43b40a7b098c0a40cd4a4a61874e5c7ed5
Ben Eklund <101027885+BenEklundCS@users.noreply.github.com>
2024-09-05 04:09:11 +0000

    Simple weather app completed, just enter a name and a city

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+import requests
 
-def index(request):
-    context = {
-        "title": "Django example",
-    }
-    return render(request, "index.html", context)
+def fun_view(request):
+    name = None
+    city = None
+    weather_data = None
+
+    if request.method == 'POST':
+        name = request.POST.get('name')  # Get the name from the form
+        city = request.POST.get('city') # Get the city from the form'
+        url = "https://wttr.in/"+city+"?format=%C+%t"  # Return only condition and temperature
+        response = requests.get(url) # Get the current temperature in Sacramento
+        weather_data = response.text  # Extract temperature as text
+    return render(request, 'index.html', {'name': name, 'weather_data': weather_data})
+
