cd2ae11acca87ab220232477664a4ea117b832cb
Ben Eklund <101027885+BenEklundCS@users.noreply.github.com>
2024-09-05 03:32:53 +0000

    init

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 from hello_world.core import views as core_views
 
 urlpatterns = [
-    path("", core_views.index),
+    #path("", core_views.index),
+    path("", core_views.index, name='fun_view'),  # Root URL, linking to fun_view
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
